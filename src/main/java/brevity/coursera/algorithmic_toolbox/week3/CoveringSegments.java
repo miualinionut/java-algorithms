d9f23e1c28e2586b3eba28d40a7f8e9bca8991eb
magdamiu <badita.magda@gmail.com>
2020-01-21 23:09:38 +0200

    week3 - challenge 4 complete

--- src/main/java/brevity/coursera/algorithmic_toolbox/week3/CoveringSegments.java
+++ src/main/java/brevity/coursera/algorithmic_toolbox/week3/CoveringSegments.java
@@ -1,27 +1,84 @@
 package brevity.coursera.algorithmic_toolbox.week3;
 
 import java.util.*;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class CoveringSegments {
 
     private static int[] optimalPoints(Segment[] segments) {
-        //write your code here
-        int[] points = new int[2 * segments.length];
-        for (int i = 0; i < segments.length; i++) {
-            points[2 * i] = segments[i].start;
-            points[2 * i + 1] = segments[i].end;
-        }
-        return points;
+
+        List<Segment> resources = IntStream.range(0, segments.length)
+                .mapToObj(i -> segments[i])
+                .sorted(Comparator.comparingInt(Segment::getEnd))
+                .collect(toList());
+
+        Deque<Integer> solutionCandidate = greedy(resources);
+
+        return solutionCandidate.stream()
+                .mapToInt(Integer::intValue)
+                .sorted()
+                .toArray();
     }
 
-    private static class Segment {
+    public static Deque<Integer> greedy(List<Segment> resources) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        Integer nextValue = greedyChoice(stack, resources);
+
+        while (isSafeMove(resources, nextValue)) {
+            makeMove(stack, nextValue);
+            reduceToSubproblem(resources, nextValue);
+            nextValue = greedyChoice(stack, resources);
+        }
+
+        return stack;
+    }
+
+    public static void makeMove(Deque<Integer> stack, Integer greedyChoice) {
+        stack.push(greedyChoice);
+    }
+
+    public static boolean isSafeMove(List<Segment> resources, Integer greedyChoice) {
+        return Objects.nonNull(greedyChoice);
+    }
+
+    public static Integer greedyChoice(Deque<Integer> stack, List<Segment> resources) {
+        Integer nonSafeValue = null;
+        Integer greedyChoiceValue = nonSafeValue;
+
+        Optional<Segment> segment = resources.stream().findFirst();
+        if (segment.isPresent()) {
+            greedyChoiceValue = getHighestValueResource(resources, segment.get());
+        }
+
+        return greedyChoiceValue;
+    }
+
+    private static Integer getHighestValueResource(List<Segment> resources, Segment current) {
+        return current.end;
+    }
+
+    public static void reduceToSubproblem(List<Segment> resources, Integer greedyChoice) {
+        List<Segment> outdatedResources = resources.stream()
+                .filter(r -> r.start <= greedyChoice && greedyChoice <= r.end)
+                .collect(toList());
+        resources.removeAll(outdatedResources);
+    }
+
+    public static class Segment {
         int start, end;
 
         Segment(int start, int end) {
             this.start = start;
             this.end = end;
         }
+
+        public int getEnd() {
+            return end;
+        }
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
