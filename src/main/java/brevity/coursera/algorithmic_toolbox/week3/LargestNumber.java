1606f1c5a9a572cac7c97615299414eeb7ff79b9
magdamiu <badita.magda@gmail.com>
2020-01-22 22:22:24 +0200

    week 3 - challenge 7

--- src/main/java/brevity/coursera/algorithmic_toolbox/week3/LargestNumber.java
+++ src/main/java/brevity/coursera/algorithmic_toolbox/week3/LargestNumber.java
@@ -1,15 +1,69 @@
 package brevity.coursera.algorithmic_toolbox.week3;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LargestNumber {
+
+    public static int maxLength = 0;
+
     private static String largestNumber(String[] a) {
-        //write your code here
-        String result = "";
-        for (int i = 0; i < a.length; i++) {
-            result += a[i];
+        maxLength = Stream.of(a).map(String::length).mapToInt(Integer::intValue).max().getAsInt();
+        List<String> resources = Stream.of(a).sorted(ProblemData.largestFirst).collect(Collectors.toList());
+        ProblemData problemData = new ProblemData(resources);
+        Deque<String> largestNumber = greedy(problemData);
+
+        return largestNumber.stream()
+                .collect(Collectors.joining());
+    }
+
+    private static Deque<String> greedy(ProblemData problemData) {
+        Deque<String> stack = new ArrayDeque<>();
+        String nextValue = greedyChoice(stack, problemData);
+        while(isSafeMove(stack, problemData, nextValue)) {
+            makeMove(stack, nextValue);
+            reduceToSubproblem(stack, problemData);
+            nextValue = greedyChoice(stack, problemData);
         }
-        return result;
+        return stack;
+    }
+
+    private static void reduceToSubproblem(Deque<String> stack, ProblemData problemData) {
+        String current = stack.element();
+        problemData.resources.remove(current);
+    }
+
+    private static void makeMove(Deque<String> stack, String nextValue) {
+        stack.push(nextValue);
+    }
+
+    private static boolean isSafeMove(Deque<String> stack, ProblemData problemData, String nextValue) {
+        return Objects.nonNull(nextValue);
+    }
+
+    private static String greedyChoice(Deque<String> stack, ProblemData problemData) {
+        return problemData.resources.stream()
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static class ProblemData {
+        private List<String> resources;
+
+        public ProblemData(List<String> resources) {
+            this.resources = resources;
+        }
+
+        public List<String> getResources() {
+            return resources;
+        }
+
+        public static Comparator<String> largestFirst = (t0, t1) -> {
+            String t0t1 = t0 + t1;
+            String t1t0 = t1 + t0;
+            return t0t1.compareTo(t1t0);
+        };
     }
 
     public static void main(String[] args) {
